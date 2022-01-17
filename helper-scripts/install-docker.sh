410668b8ecb1e69ed386707d0eb04b18e32168dc
Niklas Arnitz <niklas.arnitz@icloud.com>
2022-01-17 16:56:44 +0100

    Initial Commit

---
+++ helper-scripts/install-docker.sh
@@ -0,0 +1,9 @@
+# !/bin/bash
+
+# Install Docker
+curl -sSL https://get.docker.com/ | CHANNEL=stable sh
+systemctl enable --now docker
+
+# Install Docker-Compose
+curl -L https://github.com/docker/compose/releases/download/$(curl -Ls https://www.servercow.de/docker-compose/latest.php)/docker-compose-$(uname -s)-$(uname -m) > /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
---
+++ setup.sh
@@ -0,0 +1,7 @@
+# !/bin/bash
+
+# Install Docker
+bash ./helper-scripts/install-docker.sh
+
+# Create Docker Network
+docker network create nginxproxymanager
